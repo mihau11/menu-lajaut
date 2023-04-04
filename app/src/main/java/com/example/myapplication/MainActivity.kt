a4e5e9616052d06ec58fc9b54ab28f5330d32559
budzik12 <81246476+budzik12@users.noreply.github.com>
2023-04-04 09:29:53 +0200

    jest menu

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -12,12 +15,31 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var licznik=1
-        val visi= arrayListOf(View.VISIBLE,View.INVISIBLE)
+
+        var licznik = 1
+        val visi = arrayListOf(View.VISIBLE, View.INVISIBLE)
         findViewById<Button>(R.id.but).setOnClickListener {
-            if(findViewById<EditText>(R.id.log).text.toString()=="login" && findViewById<EditText>(R.id.pass).text.toString() == "haslo")
-            {licznik++
-            findViewById<TextView>(R.id.textView).visibility= visi[licznik%2]}
+            if (findViewById<EditText>(R.id.log).text.toString() == "login" && findViewById<EditText>(
+                    R.id.pass
+                ).text.toString() == "haslo"
+            ) {
+                licznik++
+            }
+
         }
+
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.more -> finish()
+            R.id.more2 -> startActivity(Intent(this, daneB::class.java))
+            R.id.more3 -> startActivity(Intent(this, dziennikB::class.java))
+        }
+        return true
     }
 }
---
+++ app/src/main/java/com/example/myapplication/dziennikB.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class dziennikB : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dziennik_b)
+    }
+}
